90958a27161cdbfaab28943d4aec7a53c3e5ba81
alexis-ramoss <up202404977@edu.fe.up.pt>
2026-05-05 22:46:17 +0100

    environment setup

--- auth-context.tsx
+++ auth-context.tsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useMemo, useState, type ReactNode } from "react";
 import { auth, hasRequiredConfig } from "./firebase";
-import { db } from "./firebase";
 
 type FirebaseUser = {
   uid: string;
